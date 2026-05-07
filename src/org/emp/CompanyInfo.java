af01c6cb4b2b5dca33cd898aeaefb24283bf21dd
Rsudhakaran66 <sudha666karan@gmail.com>
2026-05-07 21:41:07 +0530

    add method from karan

--- src/org/emp/CompanyInfo.java
+++ src/org/emp/CompanyInfo.java
@@ -5,9 +5,24 @@ public class CompanyInfo {
  System.out.println("comapny age is " +age +"\n company pin is "+atmPin +"\ncompany pincode is "+pincode+"\tcomapany bankBalance is "+bankBalance);
 
 	}
+	
+	private void m10() {
+System.out.println("rtytrertytr");
+	}
+	private void m22() {
+System.out.println("rtytrerfgh");
+	}
+	private void m33() {
+		// TODO Auto-generated method stub
+
+	}
 	public static void main(String[] args) {
 		CompanyInfo u = new CompanyInfo();
 		u.companyName(127,2342,65426,53221.3);
+	u.m22();
+	u.m10();
+	u.m33();
 	}
+	
 
 }
